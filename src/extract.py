f2d2e05cf1059a6390cd94c5858140d85bc399ff
Lucas Venezian Povoa <lucasvenez@gmail.com>
2022-06-01 19:28:32 -0300

    Improve software architecture.

--- src/extract.py
+++ src/extract.py
@@ -2,8 +2,6 @@ from src.constants import *
 
 import requests
 import os
-import zipfile
-import pathlib
 
 
 def create_destination_paths(destination):
@@ -13,9 +11,10 @@ def create_destination_paths(destination):
     try:
         os.makedirs(raw_path)
         os.makedirs(tables_path)
-        return raw_path, tables_path
     except FileExistsError:
         pass
+
+    return raw_path, tables_path
 
 
 def extract(destination="./data"):
@@ -24,17 +23,17 @@ def extract(destination="./data"):
 
     for file_name, source_url in SOURCE_URLS.items():
         download_file(
-            IBGE_FTP_BASE_URL.format(source_url),
+            source_url,
             os.path.join(raw_path, file_name)
         )
 
 
-def download_file(url, endereco):
+def download_file(url, address):
     # faz a requisicao ao servidor
     response = requests.get(url)
 
     if response.status_code == requests.codes.OK:
-        with open(endereco, 'wb') as new_file:
+        with open(address, 'wb') as new_file:
             new_file.write(response.content)
     else:
         response.raise_for_status()
